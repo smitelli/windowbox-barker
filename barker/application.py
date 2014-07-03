1d8d23e82463bbe689573430b311c0efed2d675c
Scott Smitelli <scott@smitelli.com>
2014-07-03 08:49:41 -0400

    Add tweepy

--- barker/application.py
+++ barker/application.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 import ConfigParser
+import tweepy
 from barker import AppPath
 from barker.models.scraper import MetadataScraper
 from barker.models.windowbox import WindowboxAPI
@@ -13,20 +14,24 @@ def run():
         'state_file': config.get('windowbox', 'state_file'),
         'timeout': config.getint('windowbox', 'timeout')}
     windowbox = WindowboxAPI(**windowbox_kwargs)
-
-    twitter_kwargs = {
-        'consumer_key': config.get('twitter', 'consumer_key'),
-        'consumer_secret': config.get('twitter', 'consumer_secret'),
-        'access_token': config.get('twitter', 'access_token'),
-        'access_token_secret': config.get('twitter', 'access_token_secret'),
-        'message_length_max': config.getint('twitter', 'message_length_max')}
-    #twitter = TwitterAPI(**twitter_kwargs)
-
-
     scraper = MetadataScraper(timeout=windowbox.timeout)
+
+    consumer_key = config.get('twitter', 'consumer_key')
+    consumer_secret = config.get('twitter', 'consumer_secret')
+    access_token = config.get('twitter', 'access_token')
+    access_token_secret = config.get('twitter', 'access_token_secret')
+    message_length_max = config.getint('twitter', 'message_length_max')
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    twitter = tweepy.API(auth)
+
+    # TODO find t.co URL length
+
     for p in windowbox.walk_new_posts():
-        message = p.get_message(20)
+        message = p.get_message(message_length_max)
         page_url = windowbox.get_post_url_for(p.id)
+        tweet_text = '{} {}'.format(message, page_url)
 
         # Windowbox has a pretty gross thundering herd bug when building new
         # image derivatives. Since Twitter is about to wail on this page, make
@@ -36,4 +41,5 @@ def run():
         image_url = page.get_twitter_image_url()
         scraper.load(image_url)
 
-        print message, page_url
+        status = twitter.update_status(status=tweet_text)
+        print status
